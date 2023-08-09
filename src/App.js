18ff04c7a69523d98362a01480576df150591a76
shirke23 <shivanishirke23@gmail.com>
2023-08-09 10:14:23 +0530

    task done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Summary from "./components/summary/Summary";
+import Button from "./components/button/Button";
+import Box1 from "./components/box/Box1";
+import Box2 from "./components/box/Box2";
+import Box3 from "./components/box/Box3";
 
 function App() {
+  const [radioValue, setRadioValue] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="container">
+      <div className="main">
+        <p className="line">Bundle & Save </p>
+        <Box1 radioValue={radioValue} setRadioValue={setRadioValue} />
+        <Box2 radioValue={radioValue} setRadioValue={setRadioValue} />
+        <Box3 radioValue={radioValue} setRadioValue={setRadioValue} />
+        <Summary />
+        <Button />
+        <p className="credit">
+          &copy; <span className="creditText"> Powered by Pumper</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
